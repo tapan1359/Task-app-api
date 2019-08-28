3e13a0947929b9f4399d8bd570d4e57829e29644
Tapan Parmar <tapanparmar@Tapans-MacBook-Pro.local>
2019-08-28 08:52:22 -0400

    second commit

--- src/models/user.js
+++ src/models/user.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
+        unique: true,
         require: true,
         trim: true,
         lowercase: true,
@@ -40,6 +41,21 @@ const userSchema = new mongoose.Schema({
         }
     }
 })
+
+userSchema.statics.findByCredentials = async(email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('Unable to login.')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login.')
+    }
+    return user
+}
 
 userSchema.pre('save', async function(next) {
     const user = this
--- src/routers/user.js
+++ src/routers/user.js
@@ -14,6 +14,16 @@ router.post('/users', async(req, res) => {
     }
 })
 
+router.post('/users/login', async(req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch (e) {
+        console.log(e)
+        res.status(400).send(e)
+    }
+})
+
 router.get('/users', async(req, res) => {
 
     try {
